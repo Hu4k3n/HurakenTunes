e577e57e8065bbd7d96d44e5cc013687567bfcd2
Hu4k3n <45396214+Hu4k3n@users.noreply.github.com>
2019-11-16 11:03:39 +0530

    normal pyrhon file to download songs

---
+++ p.py
@@ -0,0 +1,24 @@
+# ydl1.py
+from __future__ import unicode_literals
+import youtube_dl
+import sys
+from sys import *
+
+def main() :
+	#p = stdin.readline()
+	#p = p.strip('\n')
+	p='https://www.youtube.com/watch?v=aJOTlE1K90k'
+	ydl_opts = {
+            'format': 'bestaudio/best', 
+                   'format': 'bestaudio/best',
+               'postprocessors': [{
+               'key': 'FFmpegExtractAudio',
+               'preferredcodec': 'mp3',
+               'preferredquality': '320',
+             }],
+                }
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([str(p)])
+	#ydl.download([p])
+if __name__=='__main__' :
+	main()
