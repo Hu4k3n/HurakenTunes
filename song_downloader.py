7a326e2f8a5b87ecdbb7a15ff83d0aeb302faa88
Arjun Syam <45396214+Hu4k3n@users.noreply.github.com>
2019-11-21 01:10:44 +0530

    Alpha stage
    Error messeages not set

--- song_downloader.py
+++ song_downloader.py
@@ -2,20 +2,19 @@ from __future__ import unicode_literals
 from telegram.ext import Updater, CommandHandler
 import logging
 import youtube_dl
+import os
 
-updater=Updater(token='<token_number_u_have_to_put',use_context=True)
+updater=Updater(token='931539659:AAHWXmg0zDUrr6mX1GxUAnsslEOeFg6Zyk8',use_context=True)
 dispatcher=updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 def start(update,context) :
     context.bot.send_message(chat_id=update.effective_chat.id, text="This is a work of Huraken")
-def echo(update, context):
+def dl(update, context):
     p=update.message.text
-    #context.bot.send_message(chat_id=update.effective_chat.id, text=p)
-    #p = stdin.readline()
     p = p.strip('\n')
-	#p='https://www.youtube.com/watch?v=aJOTlE1K90k'
     ydl_opts = {
+            'outtmpl':'music/%(title)s.%(ext)s',
             'format': 'bestaudio/best', 
                    'format': 'bestaudio/best',
                'postprocessors': [{
@@ -24,12 +23,17 @@ def echo(update, context):
                'preferredquality': '320',
              }],
                 }
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Downloading Song...')
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([str(p)])
     
+    list=os.listdir('music')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Uploading Song...')
+    context.bot.send_audio(chat_id=update.effective_chat.id, audio=open('music/'+list[0], 'rb'))
+    os.system("rm -rf music")
 
 from telegram.ext import MessageHandler, Filters
-echo_handler = MessageHandler(Filters.text, echo)
+echo_handler = MessageHandler(Filters.text, dl)
 dispatcher.add_handler(echo_handler)
 
 start_handler=CommandHandler('start',start)
