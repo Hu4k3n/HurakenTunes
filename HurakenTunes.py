ef120426b18ced2b5133579367c0f25dc973630c
Arjun Syam <45396214+Hu4k3n@users.noreply.github.com>
2019-11-24 14:58:14 +0530

    Small changes

--- HurakenTunes.py
+++ HurakenTunes.py
@@ -3,16 +3,20 @@ from telegram.ext import Updater, CommandHandler
 import logging
 import youtube_dl
 import os
+import sys
 
-updater=Updater(token='<api token>',use_context=True)
+updater=Updater(token='931539659:AAHjbKN9qu4WG2B317kKiWebySvvwfeOHnU',use_context=True)
 dispatcher=updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 def start(update,context) :
-    context.bot.send_message(chat_id=update.effective_chat.id, text="This is a work of Huraken")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome To HurakenTunes\n\nHow to use\nShare the link of the youtube video you wish to convert\nDo wait the server is not always running\n\nSincerely,\nHuraken.")
 def dl(update, context):
     p=update.message.text
     p = p.strip('\n')
+    if ("code:stopthis1" in p) :
+        sys.exit()
+        os.system("exit")
     if (not "https://youtu.be" in p) :
         context.bot.send_message(chat_id=update.effective_chat.id, text='Wrong Link')
 
@@ -35,8 +39,11 @@ def dl(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text='Uploading Song...')
         context.bot.send_audio(chat_id=update.effective_chat.id, audio=open('music/'+list[0], 'rb'))
         os.system("rm -rf music")
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Session Timeout Try again')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Thank you for using HurakeTunes!\n Press to View : /start')
+    print ('Input : '+p)
+    print ('Content : '+list[0])
     print ('Session Complete')
+
     
 from telegram.ext import MessageHandler, Filters
 echo_handler = MessageHandler(Filters.text, dl)
